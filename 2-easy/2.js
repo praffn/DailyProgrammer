016c3c089d7ec13f7290a8cae2cc8f66900d75e0
Phillip Raffnsøe <phillip@praffn.dk>
2016-04-19 15:10:48 +0200

    add some comments

--- 2-easy/2.js
+++ 2-easy/2.js
@@ -1,3 +1,12 @@
+// simple arithmetic calculator
+// use like so:
+// node 2.js <X> <OPERATOR> <Y>
+// Legal operators:
+// + : addition
+// - : subtraction
+// x : multiplication (*/asterisk cannot be used unquoted, and thus I settled with x)
+// / : division
+
 var x = parseInt(process.argv[2], 10);
 var operator = process.argv[3];
 var y = parseInt(process.argv[4], 10);
